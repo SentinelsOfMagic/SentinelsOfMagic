19e9968a08ec990ad7bfd4c45b3e8d4e37bb3d74
Ivana <ivana.he.lili@gmail.com>
2017-05-04 15:21:07 -0700

    drafted frontend for createUser

--- client/src/CreateUser.jsx
+++ client/src/CreateUser.jsx
@@ -1,18 +1,45 @@
 import React from 'react';
+import $ from 'jquery';
+import ReactDOM from 'react-dom';
 
 class CreateUser extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: ''
+      userName: '',
+      userNameExists: false,
+      messageForUser: 'please type in a username'
     };
     this.change = this.change.bind(this);
+    this.submitUserName = this.submitUserName.bind(this);
   }
 
   change(e) {
+
     this.setState ({
-      userName: e.target.value
+      userName: e.target.value,
+      userNameExists: true
     })
+  }
+
+  submitUserName() {
+    var userName = this.state.userName;
+    if (this.state.userName === true) {
+      $.ajax({
+      method:'POST',
+      url: 'match server route',
+      data: {'userName': userName},
+      success: (data) => {
+        this.setState({
+          messageForUser: ''
+        })
+        console.log('success but i dont expect to get back any data', data);
+      },
+      dataType: 'json'
+      })
+    }
+    //if user succesfully posts to database succesfully, do we still need a next button?
+    //shouldn't we redirect them to another page right after?
   }
 
   render () {
@@ -20,16 +47,18 @@ class CreateUser extends React.Component {
       <div>
         <div>Please create one user before proceeding</div>
         <div>Username</div>
-        <input onChange={this.change} type="text">
-        <button type="submit">Submit</button>
-        <button type="submit">Next</button>
+        <input type="text" onChange={this.change}>
+        <div>{this.state.message}</div>
+        <button type="submit" onClick={this.submitUserName}>Submit</button>
       </div>
     )
   }
 
 }
 
-ReactDOM.render(<CreateUser/>, document.getElementById('app'))
+export default CreateUser;
+
+// ReactDOM.render(<CreateUser/>, document.getElementById('app'));
 
 
 
