431e7c01ae123947995ab789b02f433368e43e0b
April Ablon <ablonapril@gmail.com>
2017-05-04 15:14:03 -0700

    Create shell for HouseInventoryListItem component

---
+++ client/src/HouseInventoryListItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class HouseInventoryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state({
+
+    });
+  }
+
+  render() {
+
+  }
+}
+
+export default HouseInventoryListItem;
