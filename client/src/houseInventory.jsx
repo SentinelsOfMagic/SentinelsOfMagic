0ec00b460f4fd151af203a19cd90733a15d02876
April Ablon <ablonapril@gmail.com>
2017-05-04 11:38:19 -0700

    Add functionality to update app state with GET request to db on refresh

--- client/src/houseInventory.jsx
+++ client/src/houseInventory.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
 class HouseInventory extends React.Component {
   constructor(props) {
     super(props);
 
     this.state({
+      items: []
+    });
+  }
 
+  componentDidMount() {
+    this.getItems(this.updateItems.bind(this));
+  }
+
+  getItems(callback) {
+    $.ajax({
+      type: 'GET',
+      url: '/inventory',
+      success: function(data) {
+        console.log('Successful GET request - house inventory items retrieved');
+        callback(data);
+      },
+      error: function() {
+        console.log('Unable to GET house inventory items');
+      }
+    });
+  }
+
+  updateItems(data) {
+    this.setState({
+      items: data
     });
   }
 
