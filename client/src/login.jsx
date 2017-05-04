30340a9d84632362413736ffd691e744a2592694
kvo180 <khoavo180@gmail.com>
2017-05-04 11:25:27 -0700

    Add skeleton for LoginApp

---
+++ client/src/login.jsx
@@ -0,0 +1,1 @@
+ReactDOM.render(<LoginApp />, document.getElementById('login'));
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,12 @@ var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
+  entry: {
+    index: `${SRC_DIR}/index.jsx`,
+    login: `${SRC_DIR}/login.jsx`
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].entry.js',
     path: DIST_DIR
   },
   module : {
