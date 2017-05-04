18296c8a6229410935a30c3a61777b25a34eb484
April Ablon <ablonapril@gmail.com>
2017-05-04 12:21:05 -0700

    Add functional component to map each list item to the HouseInventoryListItem component

---
+++ client/src/HouseInventoryList.jsx
@@ -0,0 +1,16 @@
+import HouseInventoryListItem from './HouseInventoryListItem.jsx';
+
+var HouseInventoryList = (props) => {
+  return (
+    <div>
+      {props.items.map((item, index) =>
+        <HouseInventoryListItem
+        item = {item}
+        key = {index}
+        />
+      )}
+    </div>
+  );
+};
+
+export default HouseInventoryList;
