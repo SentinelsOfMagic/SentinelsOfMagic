29c77fbd0f55f06081bcd9d95a2557cae070632f
Connor K. Wilson <ckwilson42@gmail.com>
2017-05-04 16:13:58 -0700

    add ssl option to pg-promise

--- database/index.js
+++ database/index.js
@@ -1,4 +1,4 @@
-let pgp = require('pg-promise')();
+let pgp = require('pg-promise')({ssl: true});
 let databaseUrl = process.env.DATABASE_URL || require('./config');
 
 module.exports = pgp(databaseUrl);
