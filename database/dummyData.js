f417d8440e9b6c82d990015353508cfe8fd5a256
April Ablon <ablonapril@gmail.com>
2017-05-08 17:30:01 -0700

    Create dummy data to test front-end HouseInventory functionality

---
+++ database/dummyData.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    name: 'Apples',
+    notes: 'Fresh fresh apples',
+    need_to_restock: false,
+    user_id: null
+  },
+  {
+    name: 'Pears',
+    notes: 'I like pears',
+    need_to_restock: false,
+    user_id: null
+  },
+  {
+    name: 'Milk',
+    notes: 'MOO dairy',
+    need_to_restock: false,
+    user_id: null
+  },
+  {
+    name: 'Bread',
+    notes: '<3 carbs',
+    need_to_restock: false,
+    user_id: null
+  }
+];
