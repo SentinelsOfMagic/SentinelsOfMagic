7765ef386392c4854189e9eea2444fb4c7c75869
April Ablon <ablonapril@gmail.com>
2017-05-09 13:04:46 -0700

    Add axios calls to interact  with server

--- client/src/components/HouseInventoryListItem.jsx
+++ client/src/components/HouseInventoryListItem.jsx
@@ -1,61 +1,96 @@
 import React from 'react';
+import axios from 'axios';
 
 class HouseInventoryListItem extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      item: this.props.item,
+      id: this.props.item.id, // this houses_items primary key id
+      name: this.props.item.itemname,
+      notes: this.props.item.notes,
       needToRestock: this.props.item.need_to_restock,
-      userId: this.props.item.user_id,
-      username: ''
+      username: '',
+      session: '', // still unsure as to how the session hash will be accessed
+      hide: false
     };
   }
 
   clickRestock(event) {
-    // post request to the db
-    this.setState({
-      needToRestock: true
-    });
+    axios.post('/restock', {
+      itemId: this.state.id
+    })
+    .then(res => {
+      console.log('Successful POST request to /restock');
+      this.setState({
+        needToRestock: true
+      });
+    })
+    .catch(err => console.log('Bad POST request to /restock: ', err));
   }
 
   clickClaim(event) {
-    // get request to db to see who is signed in
     // post request to the db
-    this.setState({
-      userId: 1,
-      username: 'April'
-    });
+    axios.post('/claim', {
+      // send session hash
+      session: this.state.session, // would this be stored on this.state? TBD
+      itemId: this.state.id
+    })
+    .then(res => {
+      console.log('Successful POST request to /claim');
+      // response should have the username
+      // update username in state
+      this.setState({
+        username: 'April' // something like res.username
+      });
+    })
+    .catch(err => console.log('Bad POST request to /claim: ', err));
   }
 
   clickDelete(event) {
     // post request to the db
-    console.log('deleting');
+    axios.post('/delete', {
+      itemId: this.state.id
+    })
+    .then(res => {
+      console.log('Successful POST request to /delete');
+      // put in a conditional statement in the render function that "hides" the element until the page is refreshed
+      // which should update the list from the database i.e. load inventory list without the deleted item
+      this.setState({
+        hide: true
+      });
+    })
+    .catch(err => console.log('Bad POST request to /delete'));
   }
 
   render() {
-    if (!this.state.needToRestock) {
+    if (this.state.hide) {
       return (
         <div>
-          <div className="item-name">Name: {this.state.item.name}</div>
-          <div className="item-notes">Notes: {this.state.item.notes}</div>
+        </div>
+      );
+    } else if (!this.state.needToRestock) {
+      return (
+        <div>
+          <div className="item-name">Name: {this.state.name}</div>
+          <div className="item-notes">Notes: {this.state.notes}</div>
           <button type="button" className="restock-button" onClick={this.clickRestock.bind(this)}>Need to Restock</button>
         </div>
       );
-    } else if (this.state.needToRestock && this.state.userId === null) {
+    } else if (this.state.needToRestock && this.state.username === '') {
       return (
         <div>
-          <div className="item-name">Name: {this.state.item.name}</div>
-          <div className="item-notes">Notes: {this.state.item.notes}</div>
+          <div className="item-name">Name: {this.state.name}</div>
+          <div className="item-notes">Notes: {this.state.notes}</div>
           <button type="button" className="claim-button" onClick={this.clickClaim.bind(this)}>Add to My Shopping List</button>
           <button type="button" className="delete-button" onClick={this.clickDelete.bind(this)}>Delete</button>
         </div>
       );
-    } else if (this.state.needToRestock && typeof this.state.userId === 'number') {
+    } else if (this.state.needToRestock && this.state.username.length > 0) {
       return (
         <div>
-          <div className="item-name">Name: {this.state.item.name}</div>
-          <div className="item-notes">Notes: {this.state.item.notes}</div>
+          <div className="item-name">Name: {this.state.name}</div>
+          <div className="item-notes">Notes: {this.state.notes}</div>
           <button type="button" className="claimed">Claimed by {this.state.username}</button>
         </div>
       );
