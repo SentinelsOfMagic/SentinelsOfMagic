827d1759893683b027ad3cd477b825cb1e541ac5
April Ablon <ablonapril@gmail.com>
2017-05-09 19:24:20 -0700

    Add shell functions for server to recieve GET nad POST requests

--- server/index.js
+++ server/index.js
@@ -9,7 +9,29 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.get('/inventory', function(req, res) {
+  // query houses_items table
+  // use item_id to query items table
+  // use user_id to query users table
 
+  // structure data in response
+  /*
+  {
+
+  }
+  /*
+  res.send('Getting inventory...');
+});
+
+app.post('/restock', function(req, res) {
+  res.send('Need to restock item...');
+});
+
+app.post('/claim', function(req, res) {
+  res.send('Item has been claimed...');
+});
+
+app.post('/delete', function(req, res) {
+  res.send('Deleting item...');
 });
 
 app.listen(process.env.PORT || 1337, function() {
