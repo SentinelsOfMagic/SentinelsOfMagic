ea015fcb266ab4287e583bed76bf5fbbb62205e5
April Ablon <ablonapril@gmail.com>
2017-05-09 22:31:53 -0700

    Delete initialization options and change variable name

--- database/index.js
+++ database/index.js
@@ -1,5 +1,6 @@
-let pgp = require('pg-promise')({ssl: true});
-// let databaseUrl = process.env.DATABASE_URL || require('./config');
+let pgp = require('pg-promise')(); // initialization option was {ssl: true}, sorry for deleting, but wasn't working for me with it
 
-// module.exports = pgp(databaseUrl);
+let connection = process.env.DATABASE_URL || require('./config');
+
+module.exports = pgp(connection);
 
