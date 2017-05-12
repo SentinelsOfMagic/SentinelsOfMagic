db9587f5b5e6732ed2a45685a2bb60eba2122e59
April Ablon <ablonapril@gmail.com>
2017-05-12 16:59:53 -0700

    Test different material-ui buttons

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 
 const Nav = (props) => {
   return (
     <div>
-      <button id="my-shopping-list"><Link to={'/shop'}>My Shopping List</Link></button>
-      <button id="house-inventory"><Link to={'/inventory'}>House Inventory</Link></button>
+      <FlatButton id="my-shopping-list"><Link to={'/shop'}>My Shopping List</Link></FlatButton>
+      <FlatButton id="house-inventory"><Link to={'/inventory'}>House Inventory</Link></FlatButton>
     </div>
   );
 };
