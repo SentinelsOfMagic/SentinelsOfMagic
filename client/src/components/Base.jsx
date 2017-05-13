10459b1379a21e33bdcf46baab19b81f04722181
April Ablon <ablonapril@gmail.com>
2017-05-12 20:38:32 -0700

    Change to App Bar implementation

--- client/src/components/Base.jsx
+++ client/src/components/Base.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import { Router, hashHistory, Route, IndexRoute, withRouter } from 'react-router'; // here in attempt to make the title clickable
 
 
-const Base = () => (
-  <div>
+class Base extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false
+    };
+  }
+
+  handleToggle(event) {
+    this.setState({ open: !this.state.open });
+  }
+
+  handleClose() {
+    this.setState({open: false});
+  }
+
+  render() {
+    return (
+      <div>
+        <AppBar
+          onLeftIconButtonTouchTap={this.handleToggle.bind(this)}
+          onLeftIconButtonClick={this.handleToggle.bind(this)}
+          title="Fridgr"
+        />
+          <Drawer
+            docked={false}
+            open={this.state.open}
+            onRequestChange={open => this.setState({ open })}
+          >
+            <MenuItem onTouchTap={this.handleClose.bind(this)} primaryText="Login" containerElement={<Link to="/login"/>}/>
+            <MenuItem onTouchTap={this.handleClose.bind(this)} primaryText="Sign Up" containerElement={<Link to="/signup"/>}/>
+          </Drawer>
+      </div>
+    );
+  }
+}
+
+export default Base;
+/*
     <div className="top-bar">
       <div className="top-bar-left">
         <NavLink exact to="/">Fridgr</NavLink>
@@ -15,8 +57,4 @@ const Base = () => (
       </div>
 
     </div>
-
-  </div>
-);
-
-export default Base;
+*/
