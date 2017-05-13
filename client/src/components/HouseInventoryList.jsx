bf7c46834f27abd64d073b22a05c826f7fbe81d6
April Ablon <ablonapril@gmail.com>
2017-05-13 14:11:06 -0700

    Change key property to fix deletion bug

--- client/src/components/HouseInventoryList.jsx
+++ client/src/components/HouseInventoryList.jsx
@@ -4,12 +4,13 @@ import { GridList, GridTile } from 'material-ui/GridList';
 
 var HouseInventoryList = (props) => {
   return (
-    <GridList cellHeight="auto" cols="4" padding={15}>
-      {props.items.map((item, index) =>
+    <GridList cellHeight="auto" cols={4} padding={15}>
+      {props.items.map((item) =>
         <HouseInventoryListItem
-        item = {item}
-        userId = {props.userId}
-        key = {index}
+        item={item}
+        userId={props.userId}
+        key={item.id}
+        submitItem={props.submitItem}
         />
       )}
     </GridList>
