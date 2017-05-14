cd1083ccae2e054a252f124dafaffd0d8ea9f9bd
April Ablon <ablonapril@gmail.com>
2017-05-14 12:33:15 -0700

    Add display of housename in House Inventory

--- client/src/components/houseInventory.jsx
+++ client/src/components/houseInventory.jsx
@@ -13,7 +13,8 @@ class HouseInventory extends React.Component {
 
     this.state = {
       items: [],
-      page: 'inventory'
+      page: 'inventory',
+      housename: ''
     };
 
     var cookies = document.cookie.replace(/ /g, '').split(';').map(item => item.split('='));
@@ -29,19 +30,35 @@ class HouseInventory extends React.Component {
 
   componentDidMount() {
     this.submitItem();
+    this.getHouseName(this.updateHouseName.bind(this));
   }
 
   submitItem() {
     this.getItems(this.updateItems.bind(this));
   }
 
+  getHouseName(callback) {
+    axios.post('/housename', { houseId: this.state.houseId })
+      .then(res => {
+        console.log('Successful POST request to /housename');
+        callback(res.data.housename);
+      })
+      .catch(err => console.log('Unsuccessful POST request to /housename - unable to retrieve housename: ', err));
+  }
+
+  updateHouseName(data) {
+    this.setState({
+      housename: data
+    });
+  }
+
   getItems(callback) {
     axios.post('/inventory', { houseId: this.state.houseId })
       .then(res => {
-        console.log('Successful GET request to /inventory - house inventory items retrieved');
+        console.log('Successful POST request to /inventory - house inventory items retrieved');
         callback(res.data);
       })
-      .catch(err => console.log('Unable to GET house inventory items: ', err));
+      .catch(err => console.log('Unsuccessful POST request to /inventory - unable to retrieve house inventory items: ', err));
   }
 
   updateItems(data) {
@@ -52,8 +69,9 @@ class HouseInventory extends React.Component {
 
   render() {
     return (
-    <div>
+    <div className="item">
       <Nav page={this.state.page}/>
+      <h2>{this.state.housename}</h2>
       <AddItem houseId={this.state.houseId} submitItem={this.submitItem.bind(this)}/>
       <HouseInventoryList items={this.state.items} userId={this.state.userId} submitItem={this.submitItem.bind(this)}/>
     </div>
