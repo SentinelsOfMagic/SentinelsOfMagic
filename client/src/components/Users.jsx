d9d4349cad487ab25542ab995721b2c641d83b56
Ivana <ivana.he.lili@gmail.com>
2017-05-13 20:10:13 -0700

    changed userList to grid

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -7,7 +7,18 @@ const Users = (props) => {
   <div>
     <GridList cellHeight="auto" cols={5} padding={15}>
     {props.users
-     .map((user)=>(<div key={user.id}><span className="username changeColorForUserList" key={user.id} data-key={user.id} onClick={props.redirect}><img src="https://media.giphy.com/media/FxAYkQqdw63hC/giphy.gif" height="60" width="60"/>{user.username}</span><div className="hovered">This is me. Click away!</div></div>))}
+     .map((user)=>(
+
+        <div key={user.id}>
+
+          <span className="username changeColorForUserList" key={user.id} data-key={user.id} onClick={props.redirect}>
+
+            <div><img src="https://media.giphy.com/media/3o8dpbSeoqQZNvjANq/giphy.gif" height="60" width="60"/></div>{user.username}
+          </span>
+
+          <div className="hovered">This is me. Click away!</div>
+
+        </div>))}
     </GridList>
   </div>
   );
