6c016027da18892df82c7ac6d5176e2d1a26c058
Ivana <ivana.he.lili@gmail.com>
2017-05-13 19:52:28 -0700

    changed user list to grid

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -5,9 +5,9 @@ import { GridList, GridTile } from 'material-ui/GridList';
 const Users = (props) => {
   return (
   <div>
-    <GridList cellHeight="auto" cols={1} padding={15}>
+    <GridList cellHeight="auto" cols={5} padding={15}>
     {props.users
-     .map((user)=>(<div><span className="username changeColorForUserList" key={user.id} data-key={user.id} onClick={props.redirect}><img src="https://media.giphy.com/media/FxAYkQqdw63hC/giphy.gif" height="60" width="60"/>{user.username}</span><div className="hovered">This is me. Click away!</div></div>))}
+     .map((user)=>(<div key={user.id}><span className="username changeColorForUserList" key={user.id} data-key={user.id} onClick={props.redirect}><img src="https://media.giphy.com/media/FxAYkQqdw63hC/giphy.gif" height="60" width="60"/>{user.username}</span><div className="hovered">This is me. Click away!</div></div>))}
     </GridList>
   </div>
   );
