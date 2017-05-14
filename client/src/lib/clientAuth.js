b27adba988be1216a3cf4eb88b044306b62b4228
Connor K. Wilson <ckwilson42@gmail.com>
2017-05-13 20:57:53 -0700

    Refactor auth to use npm cookie parse

--- client/src/lib/clientAuth.js
+++ client/src/lib/clientAuth.js
@@ -1,10 +1,7 @@
+import {parse} from 'cookie';
 module.exports = (router) => {
   // router is props.history from a component served by react router
-  let cookies = {};
-  for (let cookie of document.cookie.split(';')) {
-    let c = cookie.trim().split('=');
-    cookies[c[0]] = c[1];
-  }
+  let cookies = parse(document.cookie);
 
   if (!(cookies.fridgrSesh && cookies.fridgrSesh.userId && cookies.fridgrSesh.houseId)) {
     router.push('/login');
