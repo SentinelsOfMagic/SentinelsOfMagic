d08e924abc2e81fde926e28254d13ddf10a3a317
April Ablon <ablonapril@gmail.com>
2017-05-14 17:17:47 -0700

    Change to class implementation to allows for conditional rendering of Nav bar if logged in

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
 import { Card, CardTitle } from 'material-ui/Card';
+import Nav from './Nav.jsx';
 
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
 
-const HomePage = () => (
-  <Card className="container">
-    <CardTitle title="Welcome to Fridgr" subtitle="Tracking groceries got you stressed? Chill." />
-  </Card>
-);
+    this.state = {
+      page: 'home'
+    };
+  }
+
+  render() {
+    if (localStorage.getItem('loggedIn') === 'true') {
+      return (
+        <div>
+          <Nav page={this.state.page} />
+          <Card className="container">
+            <CardTitle title="Welcome to Fridgr" subtitle="Tracking groceries got you stressed? Chill." />
+          </Card>
+        </div>
+      );
+    } else {
+      return (
+        <Card className="container">
+          <CardTitle title="Welcome to Fridgr" subtitle="Tracking groceries got you stressed? Chill." />
+        </Card>
+      );
+    }
+  }
+}
+
+// const HomePage = () => (
+//   <Card className="container">
+//     <CardTitle title="Welcome to Fridgr" subtitle="Tracking groceries got you stressed? Chill." />
+//   </Card>
+// );
 
 export default HomePage;
