fa333a26649ddb6d4498e710d6f60080f4ceffaa
April Ablon <ablonapril@gmail.com>
2017-05-14 21:29:58 -0700

    Add conditional rendering based on whether Submit button has been clicked

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -2,12 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const UserList = (props) => {
-  return (
-  <div>
-    {props.addUser.map((user)=>(<div className="user" key={user} onClick={props.passInCooks}>Continue as <Link to="/inventory">{user}</Link></div>))}
-    <div>or <Link to="/users">go back</Link> and select a different user.</div>
-  </div>
-  );
+  if (props.clicked) {
+    console.log('true click: ', props.addUser);
+    return (
+      <div>
+        <div>Continue as: </div>
+        {props.addUser.map((user)=>(<div key={user} onClick={props.passInCooks}><Link to="/inventory">{user}</Link></div>))}
+        <div>or <Link to="/users">go back</Link> and select a different user.</div>
+      </div>
+    );
+  } else {
+    console.log('false click');
+    return (
+      <div>
+        <div>or <Link to="/users">go back</Link> and select a different user.</div>
+      </div>
+    );
+  }
 };
 
 export default UserList;
