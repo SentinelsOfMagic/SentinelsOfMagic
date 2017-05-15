5446a9b482f3c8a0df61ea52e3125a644b758f68
April Ablon <ablonapril@gmail.com>
2017-05-15 15:05:42 -0700

    Change condition so that nothing is displayed when there are no users in a house

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -23,6 +23,7 @@ class UserList extends React.Component {
   }
 
   render() {
+    console.log('error: ', this.props.error);
     if (this.props.clicked && this.props.cookieIsSet) {
       return (
         <div>
@@ -44,7 +45,7 @@ class UserList extends React.Component {
           <div>or <Link to="/users">go back</Link> and select a different user.</div>
         </div>
       );
-    } else if (this.props.clicked && !this.props.cookieIsSet && this.props.error !== '') {
+    } else if (this.props.clicked && !this.props.cookieIsSet && this.props.error !== '' && this.props.usersExist) {
       return (
         <div>
           <div>or <Link to="/users">go back</Link> and select a different user.</div>
