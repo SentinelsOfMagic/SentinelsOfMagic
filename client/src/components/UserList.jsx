b69ec0044b9e3f68d00bd670c51c071d073d237d
April Ablon <ablonapril@gmail.com>
2017-05-14 20:15:45 -0700

    Add option to go back and select a different user

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 const UserList = (props) => {
   return (
   <div>
-    {props.addUser.map((user)=>(<div key={user} onClick={props.passInCooks}><Link to="/inventory"><div className="changeColorForUserList">{user}</div></Link></div>))}
+    {props.addUser.map((user)=>(<div className="user" key={user} onClick={props.passInCooks}>Continue as <Link to="/inventory">{user}</Link></div>))}
+    <div>or <Link to="/users">go back</Link> and select a different user.</div>
   </div>
   );
 };
