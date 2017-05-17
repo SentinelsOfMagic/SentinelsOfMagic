0bac86a3b1df981ca93ac541aae3c6ea2421bc36
Mel hsieh <mehsieh89@gmail.com>
2017-05-17 15:56:28 -0700

    added Change User component and adjust settings component (#6)
    * added new user component and routing
    
    * added Change User component and adjust settings component

--- index.ios.js
+++ index.ios.js
@@ -4,6 +4,7 @@ import Main from './ios/app/components/Main';
 import Login from './ios/app/components/Login';
 import SignUp from './ios/app/components/SignUp';
 import CreateUser from './ios/app/components/CreateUser';
+import ChangeUser from './ios/app/components/ChangeUser';
 import styles from './ios/app/style.js';
 
 
@@ -11,7 +12,8 @@ const newMobile = StackNavigator({
   Home: { screen: Login },
   SignUp: { screen: SignUp },
   Main: { screen: Main },
-  CreateUser: { screen: CreateUser }
+  CreateUser: { screen: CreateUser },
+  ChangeUser: { screen: ChangeUser }
 });
 
 AppRegistry.registerComponent('newMobile', () => newMobile);
--- ios/app/components/Settings.js
+++ ios/app/components/Settings.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
+import ChangeUser from './ChangeUser';
+import Login from './Login';
 import styles from '../style';
 
 class Settings extends Component {
+   constructor(props) {
+    super(props);
+    this.onPressChangeUser = this.onPressChangeUser.bind(this);
+    this.onPressLogOut = this.onPressLogOut.bind(this);
+  }
   
   static navigationOptions = { title: 'Settings' };
+
+  onPressChangeUser() {
+    const { navigate } = this.props.navigation;
+    navigate('ChangeUser');
+  }
+
+  onPressLogOut() {
+    const { navigate } = this.props.navigation;
+    navigate('Home');
+  }
 
   render() {
     return (
@@ -12,6 +29,16 @@ class Settings extends Component {
         <Text style={styles.welcome}>
           Settings
         </Text>
+         <Button
+          onPress={this.onPressChangeUser}
+          title="Change User"
+          color="#841584"
+        />
+        <Button
+          onPress={this.onPressLogOut}
+          title="Logout"
+          color="#841584"
+        />
       </View>
     );
   }
